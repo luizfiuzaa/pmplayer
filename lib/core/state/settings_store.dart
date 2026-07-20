3b9bebf6705cb031dbf3e1fa6240c2cdd6a769a1
Luiz Freitas-ext <luiz.freitas-ext@betchavas.com>
2026-07-20 18:01:40 -0300

    fix(lint): fix curly_braces_in_flow_control_structures in settings_store

--- lib/core/state/settings_store.dart
+++ lib/core/state/settings_store.dart
@@ -4,9 +4,13 @@ import 'package:shared_preferences/shared_preferences.dart';
 class SettingsStore extends ChangeNotifier {
   SettingsStore(this._prefs) {
     final mode = _prefs?.getString('theme_mode');
-    if (mode == 'dark') _themeMode = ThemeMode.dark;
-    else if (mode == 'light') _themeMode = ThemeMode.light;
-    else _themeMode = ThemeMode.system;
+    if (mode == 'dark') {
+      _themeMode = ThemeMode.dark;
+    } else if (mode == 'light') {
+      _themeMode = ThemeMode.light;
+    } else {
+      _themeMode = ThemeMode.system;
+    }
   }
 
   final SharedPreferences? _prefs;
@@ -15,7 +19,9 @@ class SettingsStore extends ChangeNotifier {
   ThemeMode get themeMode => _themeMode;
 
   void setThemeMode(ThemeMode mode) {
-    if (_themeMode == mode) return;
+    if (_themeMode == mode) {
+      return;
+    }
     _themeMode = mode;
     _prefs?.setString('theme_mode', mode.name);
     notifyListeners();
